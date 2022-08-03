be999842ae874a4fd1bed3e69ce31155ac994f55
Cam14uche2 <cam14uche@gmail.com>
2022-08-03 03:39:55 +0100

    membersAPP

---
+++ wiki/members/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+            # Redirect to a success page.
+        
+        else:
+            messages.success(request, ("Invalid login details, Put in correct details and try again.."))
+            # Return an 'invalid login' error message.
+            return redirect('login')
+        
+    else:
+        return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Successfully logout.."))
+    return redirect('home')
